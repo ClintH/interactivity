7259d46e39551b63b4dba12759608e86ec64747a
CH <clint@thestaticvoid.net>
2021-08-19 10:08:41 +0200

    Refreshed vector-swarm

--- canvas/vector-swarm/README.md
+++ canvas/vector-swarm/README.md
@@ -1,8 +1,12 @@
 # vector-swarm
 
-Demonstrates using vectors to move particles toward a coordinate - in this case, the pointer.
+Demonstrates using vectors to move particles toward a coordinate - in this case, the pointer. It's an extension of the [vector-motion](../vector-motion) sketch.
+
+[You can view and edit live on Glitch](https://glitch.com/edit/#!/ch-vector-swarm)
 
 ![](demo.gif)
+
+This sketch does some additional vector manipulation in order to guide things toward the current pointer location
 
 Uses:
 * [Victor.js](http://victorjs.org/) - Vector math library
@@ -10,3 +14,7 @@ Uses:
 Read more (sample this is adapted from):
 * [Interactive vector motion](https://www.khanacademy.org/computing/computer-programming/programming-natural-simulations/programming-vectors/a/interactive-vector-motion) (Khan Academy)
 
+## Things to try
+
+* Tweak parameters in `moveThing` to create different dynamics for how the dots chase the pointer
+* Can some things be better or worse at reaching the pointer? Can they have mass, for example?
--- canvas/vector-swarm/script.js
+++ canvas/vector-swarm/script.js
@@ -1,11 +1,89 @@
-let things = [];
+const things = []; // Keeps track of the particles
+// Keeps track of current pointer position
+let pointerX = 0;
+let pointerY = 0;
+
+// Create a bunch of random things
+function createThings() {
+  for (let i = 0; i < 50; i++) {
+    things.push({
+      position: new Victor(randomInt(canvasSize.width), randomInt(canvasSize.height)),
+      velocity: new Victor(random(-2, 2), random(-2, 2)),
+      accel: new Victor(0, 0),
+      size: 10,
+      colour: `hsla(${Math.floor(Math.random() * 360)},100%,50%,0.7)`
+    });
+  }
+}
+
+function draw() {
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+
+  // Fade out the entire canvas
+  ctx.fillStyle = 'rgba(255,255,255,0.3)';
+  ctx.fillRect(0, 0, canvasSize.width, canvasSize.height);
+
+  // Move and draw each thing
+  things.forEach(thing => {
+    moveThing(thing);
+    drawThing(thing, ctx);
+  });
+
+  window.requestAnimationFrame(draw);
+}
+
+// Apply changes to position/velocity for a given thing
+function moveThing(thing) {
+  // Start with a vector based on pointer coordinate
+  thing.accel = new Victor(pointerX, pointerY)
+    // Subtract current thing position, yielding the 'gap' between
+    // where the thing is and where it should be
+    .subtract(thing.position)
+    // Normalise throws away magnitude and just leaves us with direction
+    .normalize()
+    // Add a constant velocity - this helps to avoid them clumping up
+    .multiply(new Victor(0.2, 0.2));
+
+  // Add this acceleration to our current velocity
+  thing.velocity.add(thing.accel);
+
+  // Make sure things don't get too crazy
+  // If any part is greater than 5, halve it
+  thing.velocity.limit(5, 0.5); // limit(max, multiplier)
+
+  // Set new position based on calculated velocity
+  thing.position.add(thing.velocity);
+}
+
+// Draws a given thing
+function drawThing(thing, ctx) {
+  ctx.fillStyle = thing.colour;
+  ctx.beginPath();
+  ctx.arc(thing.position.x, thing.position.y, thing.size, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+function onResize() {
+  const canvas = document.getElementById('canvas');
+
+  // Size canvas to match actual pixels
+  canvas.width = document.body.offsetWidth;
+  canvas.height = document.body.offsetHeight;
+
+  // Cache size
+  canvasSize = {width: canvas.width, height: canvas.height};
+}
+
+// ---------------------
+// Helper functions
 
 function onDocumentReady() {
   // Set up event listeners
   window.addEventListener('resize', onResize);
-  document.getElementById('canvas').addEventListener('pointermove', e=>{
-    window.pointerX = e.clientX;
-    window.pointerY = e.clientY;
+  document.getElementById('canvas').addEventListener('pointermove', e => {
+    pointerX = e.clientX;
+    pointerY = e.clientY;
   });
 
   onResize(); // Manually trigger first time
@@ -14,72 +92,7 @@ function onDocumentReady() {
   // Change the function name here to pick a different demo
   window.requestAnimationFrame(draw);
 }
-
-// Create a bunch of random things
-function createThings() {
-  let canvas = document.getElementById('canvas');
-
-  for (let i=0;i<50;i++) {
-    things.push({
-      position: new Victor(randomInt(canvasSize.width),randomInt(canvasSize.height)),
-      velocity: new Victor(random(-2,2),random(-2,2)),
-      accel: new Victor(0,0),
-      size: 10,
-      colour: 'hsla(' + Math.floor(Math.random()*360) + ',100%,50%,0.7)'
-    });
-  }
-}
-
-function draw() {
-  let canvas = document.getElementById('canvas');    
-  let ctx = canvas.getContext('2d');
-
-  // Fade out the entire canvas
-  ctx.fillStyle = 'rgba(255,255,255,0.3)';
-  ctx.fillRect(0,0, canvasSize.width,canvasSize.height);
-
-  // Move and draw each thing
-  things.forEach(thing => {
-    moveThing(thing);
-    drawThing(thing,ctx);
-  });
-
-  window.requestAnimationFrame(draw);
- }
-
- function moveThing(thing) {
-  thing.accel = new Victor(window.pointerX, window.pointerY)
-    .subtract(thing.position)
-    .normalize()
-    .multiply(new Victor(0.2, 0.2));
-  
-  thing.velocity.add(thing.accel);
-  thing.velocity.limit(5, 0.5);
-  thing.position.add(thing.velocity);
-}
-
-function drawThing(thing, ctx) {
-  ctx.fillStyle = thing.colour;
-  ctx.beginPath();
-  ctx.arc(thing.position.x, thing.position.y,thing.size,0,2*Math.PI);
-  ctx.fill();
-}
-
-function onResize() {
-  var canvas = document.getElementById('canvas');
-  let ctx = canvas.getContext('2d');
-  
-  // Size canvas to match actual pixels
-  canvas.width = document.body.offsetWidth;
-  canvas.height = document.body.offsetHeight;
-
-  // Cache size
-  window.canvasSize = { width: canvas.width, height: canvas.height};
-}
-
-// ---------------------
-// Helper functions
-if (document.readyState != 'loading'){
+if (document.readyState != 'loading') {
   onDocumentReady();
 } else {
   document.addEventListener('DOMContentLoaded', onDocumentReady);
@@ -90,6 +103,7 @@ function randomInt(max, min = 0) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
 function random(max, min = 0) {
   return Math.random() * (max - min) + min;
 }
