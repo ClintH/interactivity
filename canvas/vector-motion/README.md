47cfa2be5880765ee5408e42caa0c9a591d522d3
CH <clint@thestaticvoid.net>
2021-08-19 09:44:25 +0200

    Refreshed vector-motion

--- canvas/vector-motion/README.md
+++ canvas/vector-motion/README.md
@@ -2,7 +2,18 @@
 
 Demonstrates using vectors to store a particle position and apply an angular velocity.
 
+[It's also available for live editing and viewing on Glitch](https://glitch.com/edit/#!/ch-vector-motion?path=script.js%3A63%3A0)
+
+
 ![](demo.gif)
+
+'things' are defined as having a position, velocity, size and colour. Position (x,y) is initialised as a random position on the canvas. 
+
+Velocity is also an (x,y) pair, but in this case it refers to direction. A positive _x_ value would mean that it's heading to the right, while a negative _x_ would mean that it's headed to the left. Likewise _y_ means up (positive) or down (negative). The larger the numbers, the greater the direction, or in this case - speed. 
+
+A velocity of (0,0) would mean there is no movement in any direction. A velocity of (1,1) would mean moving upwards and to the right at a magnitude of '1'. Velocity of (0.5,0.5) would be half of that magnitude, while (100,100) would be 100x and so on.
+
+In this setup, we apply velocity to position, which is defined in pixels. So the number ranges we use for velocity could roughly be thought of as 'pixels to move per loop'.
 
 Uses:
 * [Victor.js](http://victorjs.org/) - Vector math library
@@ -10,3 +21,11 @@ Uses:
 Read more (sample this is adapted from):
 * [Interactive vector motion](https://www.khanacademy.org/computing/computer-programming/programming-natural-simulations/programming-vectors/a/interactive-vector-motion) (Khan Academy)
 
+## Things to try
+
+* To get a feel for velocity, try a) setting different random ranges instead of -2,2 b) use different fixed velocity vectors
+* Rather than wandering around aimlessly, can you make it appear like falling snow?
+* What about if things slowed over time (eg friction/resistance)
+* Can random jitter be added to velocity?
+* Rather than wrapping things around the screen, another option might be to delete items that fall off the screen and insert a new random one
+* Just as each thing has a colour property which is later used in in `drawThing`, think of another property things could have to visually distinguish them
--- canvas/vector-motion/script.js
+++ canvas/vector-motion/script.js
@@ -1,58 +1,43 @@
-let things = [];
-
-function onDocumentReady() {
-  window.addEventListener('resize', onResize);
-  document.getElementById('canvas').addEventListener('pointermove', e=>{
-    // Keep track of pointer
-    window.pointerX = e.clientX;
-    window.pointerY = e.clientY;
-  });
-
-  onResize(); // Manually trigger first time
-  createThings();
-
-  // Change the function name here to pick a different demo
-  window.requestAnimationFrame(draw);
-}
+const things = [];
 
 // Create a bunch of random things
 function createThings() {
-  let canvas = document.getElementById('canvas');
+  const canvas = document.getElementById('canvas');
 
-  for (let i=0;i<50;i++) {
+  for (let i = 0; i < 50; i++) {
     things.push({
-      position: new Victor(randomInt(canvas.width),randomInt(canvas.height)),
-      velocity: new Victor(random(-2,2),random(-2,2)),
+      position: new Victor(randomInt(canvas.width), randomInt(canvas.height)),
+      velocity: new Victor(random(-2, 2), random(-2, 2)),
       size: 10,
-      colour: 'hsla(' + Math.floor(Math.random()*360) + ',100%,50%,0.7)'
+      colour: `hsla(${Math.floor(Math.random() * 360)},100%,50%,0.7)`
     });
   }
 }
 
 // Main drawing loop
 function draw() {
-  let ctx = document.getElementById('canvas').getContext('2d');
+  const ctx = document.getElementById('canvas').getContext('2d');
 
   // Fade out the entire canvas
   ctx.fillStyle = 'rgba(255,255,255,0.3)';
-  ctx.fillRect(0,0,ctx.canvas.width,ctx.canvas.height);
+  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
   // Move and draw each thing
   things.forEach(thing => {
     moveThing(thing);
-    drawThing(thing,ctx);
+    drawThing(thing, ctx);
   });
 
   window.requestAnimationFrame(draw);
- }
+}
 
- // Calculate position for a thing
- function moveThing(thing) {
-  let canvas = document.getElementById('canvas');  
+// Calculate position for a thing
+function moveThing(thing) {
+  const canvas = document.getElementById('canvas');
 
   // Compute new position
   thing.position.add(thing.velocity);
-  
+
   // Wrap around canvas if edge is hit
   if (thing.position.x > canvas.width) thing.position.x = 0;
   else if (thing.position.x < 0) thing.position.x = canvas.width;
@@ -64,20 +49,31 @@ function draw() {
 function drawThing(thing, ctx) {
   ctx.fillStyle = thing.colour;
   ctx.beginPath();
-  ctx.arc(thing.position.x, thing.position.y,thing.size,0,2*Math.PI);
+  ctx.arc(thing.position.x, thing.position.y, thing.size, 0, 2 * Math.PI);
   ctx.fill();
 }
 
 // Resize canvas to match window
 function onResize() {
-  var canvas = document.getElementById('canvas');
+  const canvas = document.getElementById('canvas');
   canvas.width = document.body.offsetWidth;
   canvas.height = document.body.offsetHeight;
 }
 
+
+function onDocumentReady() {
+  window.addEventListener('resize', onResize);
+
+  onResize(); // Manually trigger first time
+  createThings();
+
+  // Change the function name here to pick a different demo
+  window.requestAnimationFrame(draw);
+}
+
 // ---------------------
 // Helper functions
-if (document.readyState != 'loading'){
+if (document.readyState != 'loading') {
   onDocumentReady();
 } else {
   document.addEventListener('DOMContentLoaded', onDocumentReady);
